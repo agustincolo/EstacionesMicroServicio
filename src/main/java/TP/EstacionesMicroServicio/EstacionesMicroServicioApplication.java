82c18352e305ef588bc2eb114015c153f2ef0804
Agustin Nicolas Colodny <agustincolodny@gmail.com>
2023-10-28 01:06:11 -0300

    version eureka

--- src/main/java/TP/EstacionesMicroServicio/EstacionesMicroServicioApplication.java
+++ src/main/java/TP/EstacionesMicroServicio/EstacionesMicroServicioApplication.java
@@ -2,8 +2,11 @@ package TP.EstacionesMicroServicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//@EnableEurekaClient
+
 public class EstacionesMicroServicioApplication {
 
 	public static void main(String[] args) {
